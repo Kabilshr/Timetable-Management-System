17a3b6705c048e5817a849c110ae954383762270
Kabilshr <kabilshrestha06@gmail.com>
2025-12-01 11:17:08 +0545

    initialized project

---
+++ TimetableManagementSystem/src/main/java/com/mycompany/timetablemanagementsystem/TimetableManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.timetablemanagementsystem;
+
+/**
+ *
+ * @author kabil
+ */
+public class TimetableManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
