3a2e64329c51044643040c04713f8eaf2c4399da
Kabilshr <kabilshrestha06@gmail.com>
2025-12-25 15:13:55 +0545

    moved teacher table controller to controller package

--- TimetableManagementSystem/src/main/java/Controller/TeacherTableController.java
+++ TimetableManagementSystem/src/main/java/Controller/TeacherTableController.java
@@ -2,8 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package Controller;
 
+import Model.AppContext;
+import Model.Teacher;
 import java.util.ArrayList;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
